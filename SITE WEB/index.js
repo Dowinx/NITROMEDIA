b976789972c993b73fa2f1cab133a8d27d0e9b15
Shain065 <shaingruber07@gmaim.com>
2022-10-10 22:24:47 +0400

    animation operationelle

--- SITE WEB/index.js
+++ SITE WEB/index.js
@@ -8,12 +8,17 @@ const options = {
   const handleIntersect = function (entries, observer) {
     entries.forEach(function (entry) {
     if (entry.intersectionRatio > ratio) {
-        entry.target.classList.add('icon1-visible')
+       entry.target.classList.add('icon1-visible')
         observer.unobserve(entry.target)
-    }
+        
+    }     
     })
-}  
+    
+}
+ 
               
 
 const observer = new IntersectionObserver(handleIntersect, options)
-  observer.observe(document.querySelector('.icon1'))
+  document.querySelectorAll('[class*="icon-"]').forEach(function(r) {
+    observer.observe(r)
+  })
