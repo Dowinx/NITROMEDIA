73004fd5315551bcf74dc7fd5cf4a1b907844f93
Dowinx <duryondoryan@gmail.com>
2022-10-10 22:43:46 +0400

    animation page 1 réalisée

---
+++ SITE WEB/index.js
@@ -0,0 +1,24 @@
+const ratio = .1
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+  
+  const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+       entry.target.classList.add('icon1-visible')
+        observer.unobserve(entry.target)
+        
+    }     
+    })
+    
+}
+ 
+              
+
+const observer = new IntersectionObserver(handleIntersect, options)
+  document.querySelectorAll('[class*="icon-"]').forEach(function(r) {
+    observer.observe(r)
+  })
