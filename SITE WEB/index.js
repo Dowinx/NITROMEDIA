be57a2ef442d48899d8b3e65acb3fd4668b8a06d
Shain065 <shaingruber07@gmaim.com>
2022-10-10 21:52:19 +0400

    ff

--- SITE WEB/index.js
+++ SITE WEB/index.js
@@ -7,17 +7,13 @@ const options = {
   
   const handleIntersect = function (entries, observer) {
     entries.forEach(function (entry) {
-    if (entry.intersectionRatio> ratio) {
-        entry.target.classList.add('icon1-visible', "icontest")
+    if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add('icon1-visible')
         observer.unobserve(entry.target)
     }
     })
 }  
               
 
-  
-  
-  const observer = new IntersectionObserver(handleIntersect, options)
-  document.querySelectorAll('[class*="icon-"]').forEach(function(r) {
-    observer.observe(r)
-  })
+const observer = new IntersectionObserver(handleIntersect, options)
+  observer.observe(document.querySelector('.icon1'))
