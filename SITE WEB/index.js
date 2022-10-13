7e93bedbc98c62e1ba25e08a17fc82aeebb9d3aa
Dowinx <duryondoryan@gmail.com>
2022-10-13 20:52:21 +0400

    Création formulaire et animation

--- SITE WEB/index.js
+++ SITE WEB/index.js
@@ -77,7 +77,27 @@ new Typewriter(txtAnime1, {
 .pause(650)
 .start()
 
+//ANIM FORMULAIRE
+const text = document.querySelector(".form");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i=0; i < splitText.length; i++){
+    text.innerHTML += "<span>"+ splitText[i] + "</span>"
+}
 
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length){
+        complete();
+        return;
+    }
+}
 
 // // ANIM TITLE FAQ
 // const txtAnime2 = document.querySelector('#h3');
